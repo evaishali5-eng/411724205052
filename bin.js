c02edbef371fe101570f7f7e17e87007629db1f8
Vaishali <evaishali5@gmail.com>
2026-06-16 10:50:18 +0530

    JS

---
+++ bin.js
@@ -0,0 +1,72 @@
+/*console.log("Print statement")
+
+const a=20;
+const b=30;
+console.log(a+b);
+const str="vaishali"
+console.log(str)
+const arr=[1,2.4,"String",false]
+console.log(arr)
+for(let i=0;i<arr.length;i++){
+    console.log(arr[i])
+}
+arr[1]=3.1
+console.log(arr[1])
+arr.push("Vaishali")
+console.log(arr)
+
+arr.pop()
+console.log(arr)
+function example(){
+    console.log("iam good girl")
+}
+const ptag=document.getElementById("ptag")
+ptag.innerHTML="print statements"
+const input=document.getElementById("input")
+const output=document.getElementById("inputField")
+
+input.addEventListener("input",()=>{
+    output.textContent=input.value
+})
+
+const first=document.createElement("h1")
+first.textContent="Some text"
+document.body.appendChild(first)
+
+first.remove()
+console.log("A")
+console.log("B")
+console.log("C")
+
+
+const promise=new Promise((resolve,reject)=>{
+    setTimer(()=>{
+        console.log("Hi")
+    },2000)
+    set Timer(()=>{
+        console.log("Done")
+    },0)
+
+    console.log("D")
+    )
+const success=true;
+if(success){
+    console.log("Done")
+}
+else{
+    console.log("No")
+}
+})
+
+promise.then(res=>{console.log.?(res)})
+.catch(err+>console.log(err));*/
+function example(){
+    console.log("Hello");
+}
+const example1=()=>{
+    const a=1;
+    const b=2;
+    console.log(1+2);
+}
+setTimeout(example(),3000)
+setTimeout(example1(),450)
